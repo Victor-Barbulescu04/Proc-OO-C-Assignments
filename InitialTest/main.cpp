9babfd409c82f8131e5d52e6fa12c12c917ac4a8
Victor-Barbulescu04 <2004victorb@gmail.com>
2024-09-03 13:40:34 -0500

    Exercise 1 : Finished Shape of Cube

--- InitialTest/main.cpp
+++ InitialTest/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <math.h>
+#include <valarray>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int side;
+
+    cout << "Enter a number:";
+    cin >> side;
+    cout << "The size of your cube is: " << pow(side, 3);
+    cout << endl;
+
     return 0;
 }

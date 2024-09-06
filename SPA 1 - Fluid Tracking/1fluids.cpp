0858c3a8d2234c5f747e34342908e99c4a7039a9
Victor-Barbulescu04 <2004victorb@gmail.com>
2024-09-06 11:14:03 -0500

    SPA 1: Fluid Tracking Initial Commit
    -Nearly complete, ask about when we're expected to break out of the loop

---
+++ SPA 1 - Fluid Tracking/1fluids.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    string time, item;
+    int amount = 0;
+    int totalAmount = 0;
+
+    // Read first user input
+    cin >> time >> item >> amount;
+
+    // Process and continue reading inputs
+    while(cin.good()) {
+        // Add the amount to the correct running total
+        if (item == "urine" || item == "bloodloss" || item == "diarrhea") {
+            totalAmount -= amount;
+        } else {
+            totalAmount += amount;
+            // If our total fluid amount is above 1000ml, print time, item, and the current total
+            if (totalAmount >= 1000) {
+                cout << "after consuming " << item << " at "<< time << ", intake exceeds output by " << totalAmount << " ml" << endl;
+            }
+        }
+
+        cin >> time >> item >> amount;
+    }
+
+    cout << "the final fluid differential is " << totalAmount << " ml" << endl;
+}

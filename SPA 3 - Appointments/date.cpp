bed33b9300623d3f0b3db0e086557d49ff22cccc
Victor-Barbulescu <barbulescuv@msoe.edu>
2024-10-13 23:22:52 -0500

    Fixed failing test cases

--- SPA 3 - Appointments/date.cpp
+++ SPA 3 - Appointments/date.cpp
@@ -9,6 +9,7 @@
 
 #include "date.h"
 
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 using namespace std;
@@ -32,17 +33,16 @@ int Date::year() const {
 string Date::to_string() const
 {
     // ostringstreams provide an cout-like interface but store the results
-    //   in a std:string instead of files; this convert data into a string
+    //   in a std:string instead of files; this convert data into a string
     ostringstream formatted;
-    if ( _month < 10 )
-        formatted << "0";
-    formatted << _month << "/";
-    if ( _day < 10 )
-        formatted << "0";
-    formatted << _day << "/" << _year;
+
+    formatted << std::setw(2) << std::setfill('0') << _month << "/";
+    formatted << std::setw(2) << std::setfill('0') << _day << "/";
+    formatted << std::setw(4) << std::setfill('0') << _year;
 
     return formatted.str();
 }
+
 
 bool Date::earlier(Date d1, Date d2) {
     // If year is less, d1 is earlier. Return true.
